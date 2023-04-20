d11fd35d9fa9a2558511c43318cedea303746897
zahra613 <zahraaassila4@gmail.com>
2023-04-20 15:48:09 -0700

    my first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+* main - Entry point *
+* Return: Always 0 (Success)
 */
 /* this program run the size of various variables  */
 int main(void)
 {
-printf("Size of a char:%la byte(s)\n",sizeof(char));
-printf("Size of an int: %la byte(s)\n",sizeof(int));
-printf("Size of a long int:%la byte(s)\n",sizeof(long int));
-printf("Size of a long long int:%la byte(s)\n",sizeof(long long int));
-printf("Size of a float: %la byte(s)\n",sizeof(float));
+printf("Size of a char:%la byte(s)\n", sizeof(char));
+printf("Size of an int: %la byte(s)\n", sizeof(int));
+printf("Size of a long int:%la byte(s)\n", sizeof(long int));
+printf("Size of a long long int:%la byte(s)\n", sizeof(long long int));
+printf("Size of a float: %la byte(s)\n", sizeof(float));
 return (0);
 }
