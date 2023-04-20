049a173660891d79ab393276710925e112717acf
zahra613 <zahraaassila4@gmail.com>
2023-04-20 15:15:22 -0700

    my first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ printf("Size of an int: 4 byte(s)\n");
 printf("Size of a long int:4 byte(s)\n");
 printf("Size of a long long int:8 byte(s)\n");
 printf("Size of a float:4 byte(s)\n");
- return (0);
+return (0);
 }
