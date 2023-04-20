6ae8ce1143f28ba9c9262a5dcef6ed2727bee018
zahra613 <zahraaassila4@gmail.com>
2023-04-20 15:14:16 -0700

    my first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 /* this program run the size of various variables  */
 int main(void)
 {
-printf("Size of a char: byte(s)\n");
-printf("Size of an int: byte(s)\n",);
-printf("Size of a long int: byte(s)\n",);
-printf("Size of a long long int: byte(s)\n");
-printf("Size of a float: byte(s)\n");
+printf("Size of a char:1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int:4 byte(s)\n");
+printf("Size of a long long int:8 byte(s)\n");
+printf("Size of a float:4 byte(s)\n");
  return (0);
 }
