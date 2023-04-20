e25ad7f3ea19a39300d5e67e7e788f4e2359a508
zahra613 <zahraaassila4@gmail.com>
2023-04-20 14:21:16 -0700

    my first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* this program run the sentence  */
+int main {
+   printf("with proper grammar, but the outcome is a piece of art,\n");
+   return 0 ;
+}
