9006a9ca1a3199d31de7e393a29d683a7650b9f4
zahra613 <zahraaassila4@gmail.com>
2023-04-20 14:37:44 -0700

    my first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,7 @@
 /* this program run the sentence  */
 int main(void)
 {
-         printf("with proper grammar, but the outcome is a piece of art,\n");
-   return (0) ;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0) ;
 }
+
