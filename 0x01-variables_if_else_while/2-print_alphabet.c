bafedc36ecc343b794718f108ae35b43fe6b619f
zahra613 <zahraaassila4@gmail.com>
2023-04-23 11:57:34 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point *
+* Return: Always 0 (Success)
+*/
+ int main (void)
+{
+char ch1;
+for(ch1='a' ;ch1 <= 'z' ;++ch1)
+{
+putchar( ch1);
+}
+return (0);      
+}
