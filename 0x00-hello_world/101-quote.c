0ce221dfd7e1366f988b3eb94f244352a9aaaa15
zahra613 <zahraaassila4@gmail.com>
2023-04-23 04:12:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-/* this program run the sentence   */
 int main(void)
 {
-printf("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 67);
 return (1);
 }
