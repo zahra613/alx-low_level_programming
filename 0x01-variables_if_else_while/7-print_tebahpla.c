805a27a6b9f049deecc3ffa3d85e2f93e48bdd28
zahra613 <zahraaassila4@gmail.com>
2023-04-23 13:43:46 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point *
+* Return: Always 0 (Success)
+*/
+int main (void)
+{
+char ch1;
+for(ch1 = 'z'; ch1 >= 'a' ; ch1--)
+{
+putchar(ch1);
+}
+putchar('\n');
+return (0);
+}
