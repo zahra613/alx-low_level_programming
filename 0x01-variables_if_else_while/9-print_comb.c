661a9bd5c51b6fd65ea34beeb4c9cab35707d1c9
zahra613 <zahraaassila4@gmail.com>
2023-04-23 14:27:48 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point *
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9 ; i++)
+{
+putchar( i + '0' );
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
