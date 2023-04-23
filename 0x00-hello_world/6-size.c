6014521a422213461582789ae1c49312c0d5baf9
zahra613 <zahraaassila4@gmail.com>
2023-04-23 03:20:40 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 /* this program run the size of various variables  */
 int main(void)
 {
-printf("Size of a char:%la byte(s)\n", sizeof(char));
-printf("Size of an int: %la byte(s)\n", sizeof(int));
-printf("Size of a long int:%la byte(s)\n", sizeof(long int));
-printf("Size of a long long int:%la byte(s)\n", sizeof(long long int));
-printf("Size of a float: %la byte(s)\n", sizeof(float));
+printf("Size of a char:%d byte(s)\n", sizeof(char));
+printf("Size of an int: %d  byte(s)\n", sizeof(int));
+printf("Size of a long int:%d byte(s)\n", sizeof(long int));
+printf("Size of a long long int:%d  byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d  byte(s)\n", sizeof(float));
 return (0);
 }
