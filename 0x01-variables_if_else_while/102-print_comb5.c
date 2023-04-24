fd1478fef589b59ef0e675a106776377df575cb9
zahra613 <zahraaassila4@gmail.com>
2023-04-24 05:48:54 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,29 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
- */
+* main - prints all possible combinations of single-digit numbers
+* Return: Always 0 (Success)
+*/
+/*this is a program that print the compbinisation of two number with repetition  numbers*/
 int main(void)
 {
-	int z, l;
-
-	for (z = 0; z <= 99; z++)
-	{
-		for (l = 0; l <= 99; l++)
-		{
-			if (z <= 97)
-			{
-				putchar((z / 10) + '0');
-				putchar((l % 10) + '0');
-				putchar(' ');
-				putchar((z / 10) + '0');
-				putchar((l % 10) + '0');
-				if (z <= 99 || l <= 99  )
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-
-		}
-	}
-	putchar('\n');
-	return (0);
+int z;
+int l;
+for (z = 0; z <= 99; z++)
+{
+for (l = z+1; l <= 99; l++)
+{
+putchar((z / 10) + '0');
+putchar((z % 10) + '0');
+putchar(' ');
+putchar((l / 10) + '0');
+putchar((l % 10) + '0');
+if (z != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
