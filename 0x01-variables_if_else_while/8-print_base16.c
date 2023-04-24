9c94dd59e24b327f8ac2b5784e499d591946afed
zahra613 <zahraaassila4@gmail.com>
2023-04-24 00:45:45 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry piont
+* Description: 'print numbers of base 16'
+* Return: always 0
+*/
+/* this is a program to print the hexadecimal value*/
+int main(void)
+{
+int i;
+char j;
+for (i = 0; i <= 9; i++)
+{
+putchar(i +'0');
+}
+for (j = 'a'; j <= 'f'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
