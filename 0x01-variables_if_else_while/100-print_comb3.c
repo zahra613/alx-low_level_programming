23336e6003b844c4aef5aef937b342226c65f923
zahra613 <zahraaassila4@gmail.com>
2023-04-24 03:02:40 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+* main - prints all possible combinations of single-digit numbers
+* Return: Always 0 (Success)
+*/
+/*this is a program that print the compbinisation of twoo numbers*/
+int main(void)
+{
+int z;
+int l;
+for (z = 0; z <= 8; z++)
+{
+for (l = z + 1; l <= 9; l++)
+{
+putchar(z+'0');
+putchar(l+'0' );
+if (z != 8)
+{
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return 0;
+}
