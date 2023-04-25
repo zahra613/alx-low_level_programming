8af195ef0c16d205cff17c99ba334a8fc38c37b7
zahra613 <zahraaassila4@gmail.com>
2023-04-25 07:55:35 -0700

    main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* main - prints all possible combinations of single-digit numbers
+* Return: Always 0 (Success)
+*/
+int print_sign(int n)
+	{
+    
+      if(c> 0 )
+      {
+          putchar('+');
+          return(1);
+
+      }
+      if(c== 0 )
+      {
+          putchar('0');
+          return(0);
+
+      }
+       if(c< 0 )
+      {
+          putchar('-');
+          return(-1);
+
+      
+       putchar(c+'0');
+      putchar(',');
+      putchar(' ');
+      putchar('\n');
+    }
+
+}
