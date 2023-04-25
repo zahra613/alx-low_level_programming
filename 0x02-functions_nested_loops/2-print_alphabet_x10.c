52accc06750a0dd6fc46b0c50a36e423402f5b45
zahra613 <zahraaassila4@gmail.com>
2023-04-25 06:15:17 -0700

    main.h

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,21 +2,22 @@
 /**
 * main - prints all possible combinations of single-digit numbers
 * Return: Always 0 (Success)
-* WRITTEN BY BLACK
+* 
 */
+/* WRITTEN BY BLACK*/
 /*this is a program that print alphabets ten time*/
 void print_alphabet_x10(void)
 {
 char z;
 int a;
 a = 0;
-while (a < 10) 
+while (a < 10)
 {
 for (z = 'a'; z <= 'z'; z++)
 {
 _putchar(z);
 }
 a++;
-}
 _putchar('\n');
+}
 }
