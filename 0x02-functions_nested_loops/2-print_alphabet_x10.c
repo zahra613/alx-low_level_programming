b5df8c37a15c0a07b6af1198e275d217aaa2c7d7
zahra613 <zahraaassila4@gmail.com>
2023-04-25 05:58:38 -0700

    main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet 10 time
+ * written by black
+ */
+void print_alphabet(void)
+{
+	char z;
+	int a;
+	a=0;
+	while(a<10) 
+	{
+   
+    for(z='a';z<='z';z++)
+    {
+        _putchar(z);
+	a++;
+    
+   }
+    }
+    putchar('\n');
+}
