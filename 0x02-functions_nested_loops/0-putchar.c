03857ca05eb3ddc8ef1d7dcc63263d55ab8cd6d8
zahra613 <zahraaassila4@gmail.com>
2023-04-25 04:42:23 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
 #include "main.h"
 /**
-* main - print _putchar
-* is written by black
-* Return: 0
-*/
-int _putchar (char c)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
 {
-char c;
-c[25] = "_putchar";
-_putchar(c);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
