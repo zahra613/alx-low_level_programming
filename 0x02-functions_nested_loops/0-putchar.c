bf7c11397ad8c5a659b499518712240e24e439c1
zahra613 <zahraaassila4@gmail.com>
2023-04-25 05:02:47 -0700

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,14 @@
 */
 int main(void)
 {
-char c;
-c[25] = "_putchar";
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
