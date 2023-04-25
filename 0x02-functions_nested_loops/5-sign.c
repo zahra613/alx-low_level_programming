b99af84c8812184f2fe02c2587f874f9ffc4f849
zahra613 <zahraaassila4@gmail.com>
2023-04-25 12:47:14 -0700

    main.h

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,7 +9,7 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-return(1);
+return (1);
 }
 if (n == 0)
 {
@@ -20,7 +20,7 @@ if (n < 0)
 {
 _putchar('-');
 return (-1);      
-_putchar(n+'0');
+_putchar(n + '0');
 _putchar(',');
 _putchar(' ');
 _putchar('\n');
