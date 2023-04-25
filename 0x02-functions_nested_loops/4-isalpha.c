f7cc5ca5d25b6f5572b74ef0f4928d430dc99ab4
zahra613 <zahraaassila4@gmail.com>
2023-04-25 07:33:48 -0700

    main.h

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,13 +5,9 @@
 * Return: Always 0.
 */
 int _isalpha(int c)
-	{
-
-      if(c>=65 && c<=122) 
-      
-          return(1);
-	  else 
-		return (0);
-
-
-          }
+{
+if (c >= 65 && c <= 122)     
+return (1);
+else
+return (0);
+}
