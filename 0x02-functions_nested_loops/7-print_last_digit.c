0c2469054124393ee22bd95b5cafd90e359b0976
zahra613 <zahraaassila4@gmail.com>
2023-04-25 15:07:50 -0700

    main.h

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *written by black
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+	{
+    int last;
+    last =(n % 10);
+   return ("%d\n", n, last);
+}
+
