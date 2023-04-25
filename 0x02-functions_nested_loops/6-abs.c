ca4b3674d81209ded05fd9e22091feca471ea320
zahra613 <zahraaassila4@gmail.com>
2023-04-25 14:40:18 -0700

    main.h

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,21 +1,19 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
 * main - abs() of numbers
 * written by black
 * Return: Always 0.
 */
-int _abs(int n )
-	{
-	if(n<=0 ){
-   
-    return(-n);
-     }
-     
-       else {
-    return(n);
-     }
-	return(0);
+int _abs(int n)
+{
+if(n <= 0)
+{
+return (-n);
 }
-
+else
+{
+return (n);
+}
+return (0);
+}
