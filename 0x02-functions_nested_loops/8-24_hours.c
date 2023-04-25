82e5ececaf460a641349597edb36a4a3d93ec488
zahra613 <zahraaassila4@gmail.com>
2023-04-25 16:04:10 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,25 +6,19 @@
  * Return: Always 0.
  */
 void jack_bauer(void)
-	{
-    int minute;
-    int hour;
-    for (hour=0; hour<=23; hour++) {
-
-    for (minute=0;minute<=59; minute++)
-    {
-
-        _putchar((hour / 10) + '0');
-		_putchar((hour % 10) + '0');
-		_putchar(':');
-		_putchar((minute / 10) + '0');
-
-		_putchar((minute % 10) + '0');
-
-         _putchar('\n');
-    }
-        _putchar('\n');
-    }
-   
+{
+int minute;
+int hour;
+for (hour = 0; hour <= 23; hour++)
+{
+for (minute = 0; minute <= 59; minute++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
 }
-
+_putchar('\n');
+}
+}
