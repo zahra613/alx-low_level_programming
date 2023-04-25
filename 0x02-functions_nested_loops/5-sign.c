c84862c386f78f415eacfc9ef93a4598bafd796e
zahra613 <zahraaassila4@gmail.com>
2023-04-25 12:43:26 -0700

    main.h

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,29 @@
 #include "main.h"
 /**
-* main - prints all possible combinations of single-digit numbers
+* 5-sign.c -Entry point
+* description: "print alphabet in lower and uppercase"
 * Return: Always 0 (Success)
 */
 int print_sign(int n)
-	{
-    
-      if(n> 0 )
-      {
-          _putchar('+');
-          return(1);
-
-      }
-      if(n== 0 )
-      {
-          _putchar('0');
-          return(0);
-
-      }
-       if(n< 0 )
-      {
-          _putchar('-');
-          return(-1);
-
-      
-       _putchar(n+'0');
-      _putchar(',');
-      _putchar(' ');
-      _putchar('\n');
-    }
-return(0);
+{
+if (n > 0 )
+{
+_putchar('+');
+return(1);
+}
+if (n == 0 )
+{
+_putchar('0');
+return (0);
+}
+if (n < 0 )
+{
+_putchar('-');
+return (-1);      
+_putchar(n+'0');
+_putchar(',');
+_putchar(' ');
+_putchar('\n');
+}
+return (0);
 }
