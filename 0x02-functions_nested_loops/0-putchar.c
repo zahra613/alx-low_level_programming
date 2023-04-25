02e907ef2e0587ed248012f99748d742f5a8eb77
zahra613 <zahraaassila4@gmail.com>
2023-04-25 01:25:57 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of single-digit numbers
+* Return: Always 0 (Success)
+*/
+/*this is a program to print the putchart sentence*/
+int main(void)
+{
+printf("_putchar");
+printf("\n");
+return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is a nested loop project
