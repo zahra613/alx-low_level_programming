9dcf375bdefdc6bd11248aca8e140759ad97fc66
zahra613 <zahraaassila4@gmail.com>
2023-04-25 03:55:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,7 @@
  * is written by black
  * Return: 0
  */
-int main(void)
-{
+int main(void){
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -15,5 +14,4 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return(0);
-}
+return(0);}
