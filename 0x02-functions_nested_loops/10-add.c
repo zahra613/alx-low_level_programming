82be174567d0d62b11922a5b466f218a464416c3
zahra613 <zahraaassila4@gmail.com>
2023-04-25 16:51:37 -0700

    main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - check the code
+*WRITTEN BY BLACK
+* Return: Always 0.
+*/
+int add(int z, int c)
+{
+int z;
+int c;
+int sum;
+sum = z + c;
+return (sum);
+}
