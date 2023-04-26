b3b964392399d0587b463a473a0e9a3e9e20a1cf
zahra613 <zahraaassila4@gmail.com>
2023-04-25 18:44:00 -0700

    main.h

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*print_to_98 - print n to 98 counts
+* print_to_98 - print n to 98 counts
 *	separated by comma, followed
 *	by space and number should be
 *	printed in order
-*
+*@c: the charachter in ASCII code
+@n: the charachter in ASCII code
 *written by black
 * Return: Always 0.
 */
