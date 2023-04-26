6da2589fcce4b92af61699129879fb1ac0293f14
zahra613 <zahraaassila4@gmail.com>
2023-04-25 17:06:56 -0700

    main.h

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *written by black
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+for (n = 0; n <= 98; n++)
+{
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+if(n <= 97)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+return (0);
+}
