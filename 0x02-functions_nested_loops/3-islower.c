d41e21816df84a3b46e2bd71493cea6f9dfdf1e8
zahra613 <zahraaassila4@gmail.com>
2023-04-25 18:19:40 -0700

    main.h

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
 * _islower - Entry point
 * @c: the charachter in ASCII code
-*  descreption: 'print alphabets that are lower case with 1 and otherwise print 0'
+*  descreption: 'print alphabets that are lower case with 1'
 *  Return 1: "for lower case 0 for the rest".
 *   written by black
 * Return:  always 0 (success)
