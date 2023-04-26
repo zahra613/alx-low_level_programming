d4976b0677f5361eaa1e1879738675fd1962ef61
zahra613 <zahraaassila4@gmail.com>
2023-04-25 18:42:11 -0700

    main.h

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* main - check the code
-*WRITTEN BY BLACK
-* Return: Always 0.
-*/
+* add  - Entry point
+* @c: the charachter in ASCII code
+* @z: the charachter in ASCII code
+*  descreption: 'sum of wo integer'
+*  Return sum: "print sum ".
+*   written by black
+* Return:  always 0 (success)
+**/
 int add(int z, int c)
 {
 int sum;
