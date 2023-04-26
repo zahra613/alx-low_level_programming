11257987874635ca5e80d32751ccd8156876ccef
zahra613 <zahraaassila4@gmail.com>
2023-04-26 11:22:05 -0700

    0main.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,11 +9,11 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a > b && b > c)
+if (a > b && a > c)
 {
 largest = a;
 }
-else if (a > b  && c > a)
+else if (a > b && c > a)
 {
 largest = c;
 }
