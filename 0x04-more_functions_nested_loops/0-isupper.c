6ceb4124c97b3300fb59440c7dbaaaebc921a787
zahra613 <zahraaassila4@gmail.com>
2023-04-27 06:36:26 -0700

    main.h

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* -isupper - Entry point
+* _isupper - Entry point
 * description: 'this function for upper and lower alphabets'
-* @c : ' it's a decimal ascii code'
+* @c:  ascii code
 * return 1: 'if is it uppercase'
 * return 0: 'if it is lowercase'
 * Return: Always 0.
 */
-
 int _isupper(int c)
 
 {
+
 if (c == 65 && c <= 90)
 {
 return (1);
