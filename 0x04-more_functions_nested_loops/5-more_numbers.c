5b2a7bb66f4ee1f0fdc60fbc029201d78de3cb96
zahra613 <zahraaassila4@gmail.com>
2023-04-27 11:18:07 -0700

    main.h

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,22 +14,16 @@ int i;
 int j;
 j = 0;
 while (j < 10)
-{
+	{
 for (i = 0; i <= 14; i++)
 {
-if (i < 10)
+if (i >= 10)
 {
-_putchar(i + '0');
+	 _putchar(i / 10 + '0');
 }
-else
-{
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-}
+ _putchar(i % 10 + '0');
 }
 j++;
-_putchar('\n');
-
+ _putchar('\n');
 }
-_putchar('\n');
 }
