592f85d2cd938ffdbfa07b82ba50b693b613d6c9
zahra613 <zahraaassila4@gmail.com>
2023-04-27 11:21:13 -0700

    main.h

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,24 +6,22 @@
 * Description: 'print numbers to 14 ten time'
 * Return: always 0
 */
-
 void more_numbers(void)
 {
-
 int i;
 int j;
 j = 0;
 while (j < 10)
-	{
+{
 for (i = 0; i <= 14; i++)
 {
 if (i >= 10)
 {
-	 _putchar(i / 10 + '0');
+_putchar(i / 10 + '0');
 }
- _putchar(i % 10 + '0');
+_putchar(i % 10 + '0');
 }
 j++;
- _putchar('\n');
+_putchar('\n');
 }
 }
