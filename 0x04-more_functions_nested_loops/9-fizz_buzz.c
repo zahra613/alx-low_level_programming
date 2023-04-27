538931fdc1267877f0f74d7a589eec5794cee571
zahra613 <zahraaassila4@gmail.com>
2023-04-27 16:22:15 -0700

    main.h

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,11 +2,10 @@
 /**
 * main - 'prints fizz if it multiple of three print buzz if it's one of multi 5'
 * written by black
-* @z: integer
 * Return: Always 0 (Success)
 */
 int main(void)
-{	
+{
 int z;
 for (z = 1; z <= 100; z++)
 {
@@ -19,7 +18,7 @@ else if (z % 5 == 0)
 printf("Buzz");
 }
 else if (z % 3 == 0)
-{	
+{
 printf("Fizz");
 }
 else
