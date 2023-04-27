cf0745a5f929f09ac98b666e63e1419dca567a2e
zahra613 <zahraaassila4@gmail.com>
2023-04-27 10:45:22 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+*  more_numbers  - Entry piont
+* Description: 'print numbers to 14'
+* Return: always 0
+*/
+/* this is a program to print the hexadecimal value*/
+void more_numbers(void)
+
+{
+
+int i;
+int j;
+while (j < 10)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i < 10)
+{
+_putchar(i + '0');
+}
+else
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+}
+j++;
+_putchar('\n');
+
+}
+}
