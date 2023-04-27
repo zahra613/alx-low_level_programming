c1c2e84965ce249db3d8843e0a739f4688c64fbe
zahra613 <zahraaassila4@gmail.com>
2023-04-27 15:23:01 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+/**
+* main - 'prints fizz if it multiple of three print buzz if it's one of multi 5'
+* written by black
+* @z: integer
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int z;
+int l;
+for (z = 0; z <= 99; z++)
+{
+    if(z!=0 && z<=99)
+    {
+
+      if (z%3==0 && z%5==0)
+      {
+
+printf(" FizzBuzz");
+putchar(' ');
+}
+ else if  (z%3==0 )
+ {
+printf("Fizz");
+putchar(' ');
+}
+ else if (z%5==0)
+ {
+printf ("Buzz");
+putchar(' ');
+}
+          else
+           {
+            printf("%d ", z);
+            putchar(' ');
+           }
+}
+
+}
+putchar('\n');
+return (0);
+}
