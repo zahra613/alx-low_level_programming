6890023cce0ded8bb4c4bdc997cde46dc27f570a
zahra613 <zahraaassila4@gmail.com>
2023-04-27 16:19:53 -0700

    main.h

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,25 +6,25 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{
+{	
 int z;
 for (z = 1; z <= 100; z++)
 {
 if (z % 3 == 0 && z % 5 == 0)
 {
-printf(" FizzBuzz");
-}
-else if  (z % 3 == 0 )
-{
-printf("Fizz");
+printf("FizzBuzz");
 }
 else if (z % 5 == 0)
 {
-printf ("Buzz");
+printf("Buzz");
+}
+else if (z % 3 == 0)
+{	
+printf("Fizz");
 }
 else
 {
-printf("%d ", z);
+printf("%d", z);
 }
 if (z != 100)
 printf(" ");
