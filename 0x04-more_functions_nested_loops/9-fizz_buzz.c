9f577b6d6a74847bc01dd8f49450a97b097e9c2d
zahra613 <zahraaassila4@gmail.com>
2023-04-27 16:14:58 -0700

    main.h

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,28 +8,26 @@
 int main(void)
 {
 int z;
-for (z = 1; z < 100; z++)
+for (z = 1; z <= 100; z++)
 {
 if (z % 3 == 0 && z % 5 == 0)
 {
 printf(" FizzBuzz");
-putchar(' ');
 }
 else if  (z % 3 == 0 )
 {
 printf("Fizz");
-putchar(' ');
 }
 else if (z % 5 == 0)
 {
 printf ("Buzz");
-putchar(' ');
 }
 else
 {
 printf("%d ", z);
-putchar(' ');
 }
+if (z != 100)
+printf(" ");
 }
 printf("\n");
 return (0);
