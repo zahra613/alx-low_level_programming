831a6e641eafa6ba7627a16427634cd8fe6d1743
zahra613 <zahraaassila4@gmail.com>
2023-04-27 07:38:50 -0700

    main.h

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 /**
 * _isupper - Entry point
-* description : 'this function for upper and lower alphabets'
+* description: 'this function for upper and lower alphabets'
 * @c: ascii code
-*  return 1 : if is it uppercase
-*  return 0 : if it is lowercase
-* Return : Always 0 (success).
+*  Return 1 : if is it uppercase
+*  Return 0 : if it is lowercase
+* Return: Always 0 (success).
 */
 int _isupper(int c)
 
