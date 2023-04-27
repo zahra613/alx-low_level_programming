01e112b3e0756afd449a35f6b15a921741335773
zahra613 <zahraaassila4@gmail.com>
2023-04-27 07:33:23 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* multi  - Entry point
+* @b: the charachter in ASCII code
+* @a: the charachter in ASCII code
+*  descreption: 'multi of two integer'
+*  Return multi: "print multiplication ".
+*   written by black
+* Return:  always 0 (success)
+**/
+int mul(int a, int b)
+
+{
+
+int multi;
+multi = a * b;
+return (multi);
+
+}
