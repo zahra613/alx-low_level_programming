3a5cce0903258ecaaff350bd349eeca38b92f3c7
zahra613 <zahraaassila4@gmail.com>
2023-04-27 14:16:37 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+* print_square  - Entry point
+*  descreption: 'function that prints a square, followed by a new line'
+*  @size: integer
+*   written by black
+* Return:  always 0 (success)
+**/
+void print_square(int size)
+{
+int square;
+int z;
+if (size <= 0)
+{
+_putchar('\n');
+}
+for (square = 0; square < size ; square++)
+{
+for (z = 0; z < size ; z++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
