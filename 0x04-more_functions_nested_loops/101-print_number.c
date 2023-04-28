76836bc1ecbf6d5ffa7f2c508284f430b5c71593
zahra613 <zahraaassila4@gmail.com>
2023-04-28 09:55:38 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_number - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+/* this program print integer  */
+void print_nuimber(int n)
+	{
+	_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+    _putchar('\n');
+    
+  
+}
+
