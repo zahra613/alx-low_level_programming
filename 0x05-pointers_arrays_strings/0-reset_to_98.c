7329b4904106cdbb8f764049eab647c98cd223f3
zahra613 <zahraaassila4@gmail.com>
2023-05-02 02:29:49 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point 
+ * reset_to_98 - it update the value of an integer
+ * @n : it's a pointer
+ *written by black 
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+	{
+
+        *n=98;
+}
+
