b9ee5aa8aad7a5edf5efe6b35601a2d9ca577614
zahra613 <zahraaassila4@gmail.com>
2023-05-02 04:29:09 -0700

    main.h

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,20 @@
 #include "main.h"
 #include <string.h>
 /**
- * main - check the code
- *
+ * print_rev  - Entry point
+ * @s : the content of a pointer
+ * written by black
  * Return: Always 0.
  */
 void print_rev(char *s)
 
 {
 
-int z ;
+int z;
 int len;
 len = strlen(s);
-z = len-1;
-while (z>=0)
+z = len - 1;
+while (z >= 0)
 {
 _putchar(s[z]);
 z--;
