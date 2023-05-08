dd5e21a6f70242daf89b430ad6af1a644f3c2bc3
zahra613 <zahraaassila4@gmail.com>
2023-05-08 08:50:04 -0700

    main.h

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *  _strstr -  function that locates a substring.
+ *@haystack : first byte memory
+ *@needle : canstant byte.
+ * Return: return haystack or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+while (*haystack != '\0')
+{
+if (*haystack == *needle)
+{
+return (haystack);
+}
+haystack++;
+}
+return (NULL);
+}
