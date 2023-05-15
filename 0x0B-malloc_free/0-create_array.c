22d6959fbf72e73324d1c86bee5c8f6a77b9ecee
zahra613 <zahraaassila4@gmail.com>
2023-05-15 11:44:02 -0700

    main.h

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - check the code for ALX School students.
+ *@size : size of array T
+ *@c : it's ana character
+ * Return: T
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *T;
+	unsigned int i;
+
+	T = malloc(size * sizeof(char));
+	if (T == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 1 ; i < size ; i++)
+	{
+		if (size == 0)
+		{
+			return (NULL);
+			}
+		*(T + i) = c;
+		}
+	return (T);
+	free(T);
+}
