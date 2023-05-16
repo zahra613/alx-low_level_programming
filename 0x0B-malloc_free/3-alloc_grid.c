a4ebd63e5c01557f41c9e678e23662f93970f3ec
zahra613 <zahraaassila4@gmail.com>
2023-05-15 19:19:08 -0700

    main.h

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
         return NULL;
     }
 
-    data = calloc(width * height, sizeof(int));
+    data = malloc((width * height)* sizeof(int));
     if (data == NULL) {
         free(grid);
         return NULL;
