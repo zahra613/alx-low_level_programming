f35c0794d23ae554610802f38b58afbfabd452cf
zahra613 <zahraaassila4@gmail.com>
2023-05-18 14:02:32 -0700

    main.h

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,9 +21,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (i = 0 ; i < (nmemb * size) ; i++)
+	for (i = 0 ; i <= (nmemb * size) ; i++)
 	{
-		memb[i] = 0;
+		*(memb + i) = 0;
 	}
 	return (memb);
 }
