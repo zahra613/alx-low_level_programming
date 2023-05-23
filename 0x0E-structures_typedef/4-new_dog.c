653b512da1f9ff3cb2b5b4ec8b6b7af6aadc8332
zahra613 <zahraaassila4@gmail.com>
2023-05-23 07:30:40 -0700

    main.h

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 /**
  * new_dog -  function that creates a new dog
@@ -9,24 +8,45 @@
  *@owner : owner of dog
  * Return:
  */
+int my_strlen(const char *str)
+{
+	int len;
+	len = 0;
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i;
 	dog_t *dog;
 	dog = malloc(sizeof(dog_t));
-
-	dog->name = strdup(name);
-	dog->owner = strdup(owner);
 	if (dog == NULL)
+	{
+		return(NULL);
+	}
+	dog->name = malloc((my_strlen(name)+1) * sizeof(char));
+	dog->owner = malloc((my_strlen(owner)+1) * sizeof(char));
+	if (dog == NULL)
+	{
+		free (dog);
+		free (dog->owner);
+		free (dog->name);
+		return (NULL);
+	}
+	for  (i = 0 ;  *(name + i) != '\0'; i++)
+	{
+		dog->name[i] = *(name +i);
+	}
+	dog->name[i] = '\0';
+	for  (i = 0 ; *(owner + i) != '\0'; i++)
         {
-                return (NULL);
+                dog->owner[i] = *(owner +i);
         }
-	if (dog->name == NULL || dog->owner == NULL)
-        {
-                free(dog->name);
-        free(dog->owner);
-        free(dog);
-        return (NULL);
-        }
+	dog->owner[i] = '\0';
 	dog->age = age;
 	return (dog);
+
 }
