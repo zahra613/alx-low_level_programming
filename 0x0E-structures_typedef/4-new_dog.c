3cb60e1c087abf71e78ca13a7558cb2ec0fa26ec
zahra613 <zahraaassila4@gmail.com>
2023-05-23 07:42:46 -0700

    main.h

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,49 +2,58 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * new_dog -  function that creates a new dog
- *@name : name of dog
- *@age : age of dog
- *@owner : owner of dog
- * Return:
+ * my_strlen -  function that calculate lengh of name and owner
+ *@str : constantant char
+ * Return:len
  */
 int my_strlen(const char *str)
 {
 	int len;
+
 	len = 0;
+
 	while (*(str + len) != '\0')
 	{
 		len++;
 	}
 	return (len);
 }
+/**
+ * new_dog -  function that creates a new dog
+ *@name : name of dog
+ *@age : age of dog
+ *@owner : owner of dog
+ * Return: dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i;
 	dog_t *dog;
+
 	dog = malloc(sizeof(dog_t));
+
 	if (dog == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
-	dog->name = malloc((my_strlen(name)+1) * sizeof(char));
-	dog->owner = malloc((my_strlen(owner)+1) * sizeof(char));
+	dog->name = malloc((my_strlen(name) + 1) * sizeof(char));
+	dog->owner = malloc((my_strlen(owner) + 1) * sizeof(char));
 	if (dog == NULL)
 	{
-		free (dog);
-		free (dog->owner);
-		free (dog->name);
+		free(dog);
+		free(dog->owner);
+		free(dog->name);
 		return (NULL);
 	}
 	for  (i = 0 ;  *(name + i) != '\0'; i++)
 	{
-		dog->name[i] = *(name +i);
+		dog->name[i] = *(name + i);
 	}
 	dog->name[i] = '\0';
 	for  (i = 0 ; *(owner + i) != '\0'; i++)
-        {
-                dog->owner[i] = *(owner +i);
-        }
+	{
+		dog->owner[i] = *(owner + i);
+	}
 	dog->owner[i] = '\0';
 	dog->age = age;
 	return (dog);
