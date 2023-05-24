bbe30a21ce4d6e0a220d3c294a192ac3f89adc85
zahra613 <zahraaassila4@gmail.com>
2023-05-24 06:10:58 -0700

    main.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,13 @@
 #ifndef  _function_pointers_h_
 #define _function_pointers_h_
-int _putchar(char c);
+
+
 #include <stddef.h>
+
+
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
