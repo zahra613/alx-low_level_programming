3a1eb5d018760dedd0e19ef807a3d3f378d1057b
zahra613 <zahraaassila4@gmail.com>
2023-05-25 06:58:25 -0700

    main.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef _variadic_functions_h_
+#define _variadic_functions_h_
+
+#include <unistd.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
