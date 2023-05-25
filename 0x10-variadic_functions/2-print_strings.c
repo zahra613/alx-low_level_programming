9be41d68c278b9042592c9918c7ed8779efa1eb9
zahra613 <zahraaassila4@gmail.com>
2023-05-25 11:11:02 -0700

    main.h

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		const char *str = va_arg(args, const char *);
 
-		if (str == NULL)
+		if (str != NULL)
 		{
 			 printf("%s", str);
 		}
