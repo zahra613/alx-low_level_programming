54a3308503a105f4a21147286d4f04f955bd2cf1
zahra613 <zahraaassila4@gmail.com>
2023-06-01 15:09:34 -0700

    main.H

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,13 +4,15 @@
  * @head: the head of the list
  * Return:
  */
-void free_list(list_t* head)
+void free_list(list_t *head)
 {
 	list_t  *temp = head;
 	list_t *next;
+
 	while (temp != NULL)
 	{
 		next = temp->next;
+		free(temp->str);
 		free(temp);
 		temp = next;
 	}
