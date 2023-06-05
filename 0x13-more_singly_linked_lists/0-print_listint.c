0eb75b8382cb74d830314b9ba8ebcaf60decdd81
zahra613 <zahraaassila4@gmail.com>
2023-06-05 10:05:18 -0700

    task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,15 @@
 */
 size_t print_listint(const listint_t *h)
 {
+	size_t counter;
 
 	if (h == NULL)
 	{
 		return (0);
 	}
+	counter = 0;
 	while (h != NULL)
 	{
-		size_t counter = 0;
 		printf("%i\n", h->n);
 		counter++;
 		h = h->next;
