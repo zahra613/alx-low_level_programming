a6897fa4970522c9d3fbf998e377f683160cabe5
zahra613 <zahraaassila4@gmail.com>
2023-06-05 10:56:35 -0700

    task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,14 +3,15 @@
  * print_listint - function that prints all the elements of a listint_t list
  * @h : it's a pointer that pointer on the head of the list
  * Return: return the elements of the list
-*/
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t counter;
 	if (h == NULL)
+	{
 
 		return (0);
-
+	}
 	counter = 0;
 	while (h)
 	{
