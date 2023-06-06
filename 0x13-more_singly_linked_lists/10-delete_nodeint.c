e002a793994d85d1681d79768ac945194205e9a6
zahra613 <zahraaassila4@gmail.com>
2023-06-06 15:34:02 -0700

    waaa3

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
+
 /**
- * delete_nodeint_at_index - function that deletes the node at index
- *@head : head of the list
- *@index : position in the list
- *Return:1
+ * delete_nodeint_at_index -  function that deletes the node at index
+ * @head: head of the list
+ * @index: position of the node to be deleted
+ * Return: 1
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current = *head;
-	listint_t *node_to_delete = current->next;
+	listint_t *previous_node = NULL;
+	listint_t *current_node = *head;
 	unsigned int i;
-	if (*head == NULL)
-	{
+
+	if (!*head || !head)
 		return (-1);
-	}
 
 	if (index == 0)
 	{
@@ -23,21 +23,20 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (i = 0; i < index - 1; i++)
+	for (i = 0; i < index; i++)
 	{
-		if (current->next == NULL)
-		{
+		if (!current_node)
 			return (-1);
-		}
-		current = current->next;
+		previous_node = current_node;
+		current_node = current_node->next;
 	}
 
-	if (node_to_delete == NULL)
-	{
+	if (!current_node)
 		return (-1);
-	}
 
-	current->next = node_to_delete->next;
-	free(node_to_delete);
+	previous_node->next = current_node->next;
+	free(current_node);
+	current_node = NULL;
 	return (1);
 }
+
