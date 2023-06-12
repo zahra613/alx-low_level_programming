b7d3ed09e95fd976c068774865529e61fd4c58ea
zahra613 <zahraaassila4@gmail.com>
2023-06-12 16:26:34 -0700

    task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,18 +6,20 @@
 #include <fcntl.h>
 #include <elf.h>
 
+/* Function to check if the file is an ELF file */
 int isElfFile(int fd) {
-    char ident[4];
-    ssize_t bytes_read = read(fd, ident, sizeof(ident));
-    if (bytes_read != sizeof(ident))
+    Elf64_Ehdr header;
+    ssize_t bytes_read = read(fd, &header, sizeof(header));
+    if (bytes_read != sizeof(header))
         return 0;
 
-    return (ident[0] == ELFMAG0 &&
-            ident[1] == ELFMAG1 &&
-            ident[2] == ELFMAG2 &&
-            ident[3] == ELFMAG3);
+    return (header.e_ident[EI_MAG0] == ELFMAG0 &&
+            header.e_ident[EI_MAG1] == ELFMAG1 &&
+            header.e_ident[EI_MAG2] == ELFMAG2 &&
+            header.e_ident[EI_MAG3] == ELFMAG3);
 }
 
+/*Function to print ELF header information*/
 void printElfHeaderInfo(const Elf64_Ehdr *header) {
     printf("%-35s%s\n", "Magic:", "ELF");
     printf("%-35s%s\n", "Class:", (header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
@@ -25,7 +27,8 @@ void printElfHeaderInfo(const Elf64_Ehdr *header) {
     printf("%-35s%d (current)\n", "Version:", header->e_ident[EI_VERSION]);
     printf("%-35s%s\n", "OS/ABI:", "UNIX - System V");
     printf("%-35s%d\n", "ABI Version:", header->e_ident[EI_ABIVERSION]);
-    printf("%-35s", "Type: ");
+
+    printf("%-35s", "Type:");
     switch (header->e_type) {
         case ET_NONE:
             printf("NONE (No file type)\n");
@@ -45,6 +48,7 @@ void printElfHeaderInfo(const Elf64_Ehdr *header) {
         default:
             printf("<unknown>\n");
     }
+
     printf("%-35s0x%lx\n", "Entry point address:", header->e_entry);
 }
 
@@ -52,13 +56,14 @@ int main(int argc, char **argv)
 {
 	int fd;
 	Elf64_Ehdr header;
-	ssize_t bytes_read;
+    ssize_t bytes_read;
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
         exit(98);
     }
 
-     fd = open(argv[1], O_RDONLY);
+    fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
         fprintf(stderr, "Error: unable to open file %s\n", argv[1]);
         exit(98);
@@ -72,7 +77,7 @@ int main(int argc, char **argv)
 
     lseek(fd, 0, SEEK_SET);
 
-   bytes_read = read(fd, &header, sizeof(header));
+    bytes_read = read(fd, &header, sizeof(header));
     if (bytes_read != sizeof(header)) {
         fprintf(stderr, "Error: unable to read ELF header of %s\n", argv[1]);
         close(fd);
