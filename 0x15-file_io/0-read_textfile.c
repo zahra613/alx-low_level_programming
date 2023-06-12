4625eb0df4e468399725cb0833ac2d8fb9868dbc
zahra613 <zahraaassila4@gmail.com>
2023-06-12 07:33:40 -0700

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -29,18 +29,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	bytes_to_read = read(file, buffer, letters);
-	if (bytes_to_read != letters)
-	{
-		free(buffer);
-		close(file);
-		return (0);
-	}
+
 	bytes_to_write = write(STDOUT_FILENO, buffer, bytes_to_read);
 	free(buffer);
-	if (bytes_to_write != bytes_to_read)
-	{
-		return (0);
-	}
+	close(file);
 	return (bytes_to_write);
 
 }
