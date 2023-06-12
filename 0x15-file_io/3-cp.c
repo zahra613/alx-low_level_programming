a8991bd07cd2a5cc992e90b6302ba689640fac31
zahra613 <zahraaassila4@gmail.com>
2023-06-12 14:47:10 -0700

    task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,29 +1,5 @@
 #include "main.h"
 /**
- * error_exit - function that prints an error message
- *@code : exit code
- *@message : error msg
- * Return: -
- */
-void error_exit(int code, const char *message)
-{
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(code);
-}
-/**
- * close_file - function that closes a file descriptor
- *@fd : file descriptor
- * Return: -
- */
-void close_file(int fd)
-{
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-/**
  * main - check the code
  *@argc : integer represent number of arguments
  *@argv : string contains arguments
@@ -49,7 +25,7 @@ int main(int argc, char *argv[])
 	if (f_wr < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close_file(f_rd);
+		close(f_rd);
 		exit(99);
 	}
 	while ((bytes_read = read(f_rd, buffer, BUFSIZ)) > 0)
@@ -58,7 +34,7 @@ int main(int argc, char *argv[])
 		if (bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close_file(f_rd);
+			close(f_rd);
 			exit(99);
 		}
 	}
