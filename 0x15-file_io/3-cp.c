d8d71b18cf32891f8295f6a69e735705e3d5be42
zahra613 <zahraaassila4@gmail.com>
2023-06-12 15:15:48 -0700

    task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,17 +13,17 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf("Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	f_rd = open(argv[1], O_RDONLY);
 	if (f_rd < 0)
 	{
-		printf("Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	f_wr = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (f_wr < 0)
 	{
-		printf("Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(f_rd), exit(99);
 	}
 	while ((bytes_read = read(f_rd, buffer, BUFSIZ)) > 0)
@@ -31,13 +31,13 @@ int main(int argc, char *argv[])
 		bytes_written = write(f_wr, buffer, bytes_read);
 		if (bytes_written != bytes_read)
 		{
-			printf("Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(f_rd), exit(99);
 		}
 	}
 	if (bytes_read < 0)
 	{
-		printf("Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	if (close(f_rd) < 0 || close(f_wr) < 0)
 	{
